f149d97707a348c0b4df82ae3475946cc65ee5ad
Philipp Heinrich <philipp.heinrich@fau.de>
2022-11-30 15:55:03 +0100

    make it installable via stetup.py

--- setup.py
+++ setup.py
@@ -1,2 +1,54 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
 
-# Tools for processing VRT files and CWB import/export")
+import os
+from setuptools import setup
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+# description
+with open(os.path.join(here, 'README.md'), mode='rt', encoding='utf-8') as f:
+    long_description = f.read()
+
+# version
+version = {}
+with open(os.path.join(here, 'vrt', 'version.py'), mode='rt', encoding='utf-8') as f:
+    exec(f.read(), version)
+
+
+setup(
+    name="cwb-vrt",
+    version=version["__version__"],
+    description="Tools for processing VRT files and CWB import/export",
+    license='GNU General Public License v3 or later (GPLv3+)',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Philipp Heinrich",
+    author_email="philipp.heinrich@fau.de",
+    url="https://github.com/ausgerechnet/cwb-vrt",
+    packages=[
+        'vrt'
+    ],
+    scripts=[
+        'bin/vrt-cohorts',
+        'bin/vrt-cqpweb',
+        'bin/vrt-index',
+        'bin/vrt-merge',
+        'bin/vrt-meta',
+    ],
+    python_requires='>=3.6.2',
+    install_requires=[
+        "pandas>=1.1.5",
+        "tqdm>=4.64.0",
+    ],
+    classifiers=[
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Development Status :: 3 - Alpha",
+        "Operating System :: Unix",
+        "Programming Language :: Python :: 3",
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
