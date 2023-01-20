f4ed4fabd414515f40c23fa466e0764035d07e24
Philipp Heinrich <philipp.heinrich@fau.de>
2023-01-20 13:46:55 +0100

    allow reading from several files when cohorting

--- vrt/cohorts.py
+++ vrt/cohorts.py
@@ -4,15 +4,16 @@
 import gzip
 import os
 from collections import defaultdict
+from glob import glob
 from tempfile import TemporaryDirectory
 
 from vrt.utils import MultiFileWriter, Progress, save_path_out
 from vrt.vrt import dict2meta, iter_s
 
 
-def process_path(path_in, path_out, force, tag_old, tag_new, cohort, categorical, memory=False):
+def process_paths(paths_in, path_out, force, tag_old, tag_new, cohort, categorical, memory=False):
 
-    f_name, path_out = save_path_out(path_in, path_out, suffix='-cohorts.vrt.gz', force=force)
+    f_name, path_out = save_path_out(paths_in[0], path_out, suffix='-cohorts.vrt.gz', force=force)
 
     if memory:
 
@@ -20,21 +21,23 @@ def process_path(path_in, path_out, force, tag_old, tag_new, cohort, categorical
         cohorts = defaultdict(list)
         cohorts_id = list()
         cohorts_meta = dict()
-        pb = Progress()
-        with gzip.open(path_in, "rt") as f_in:
-            for text, meta in iter_s(f_in, level=tag_old):
-                cohort_id = "_".join([meta[c] for c in categorical])
-                cohort_meta = {c: meta[c] for c in categorical}
-                path = os.path.join(cohort_id + ".vrt.gz")
-                if cohort_id not in cohorts_meta.keys():
-                    cohorts_meta[cohort_id] = cohort_meta
-                    cohorts_meta[cohort_id]['id'] = cohort_id
-                    cohorts_id.append(cohort_id)
-                cohorts[cohort_id].append(dict2meta(meta, level=tag_new))
-                cohorts[cohort_id].append("\n".join(text) + "\n")
-                cohorts[cohort_id].append(f"</{tag_new}>" + "\n")
-                pb.up()
-        pb.fine()
+        for path_in in paths_in:
+            print(path_in)
+            pb = Progress()
+            with gzip.open(path_in, "rt") as f_in:
+                for text, meta in iter_s(f_in, level=tag_old):
+                    cohort_id = "_".join([meta[c] for c in categorical])
+                    cohort_meta = {c: meta[c] for c in categorical}
+                    path = os.path.join(cohort_id + ".vrt.gz")
+                    if cohort_id not in cohorts_meta.keys():
+                        cohorts_meta[cohort_id] = cohort_meta
+                        cohorts_meta[cohort_id]['id'] = cohort_id
+                        cohorts_id.append(cohort_id)
+                    cohorts[cohort_id].append(dict2meta(meta, level=tag_new))
+                    cohorts[cohort_id].append("\n".join(text) + "\n")
+                    cohorts[cohort_id].append(f"</{tag_new}>" + "\n")
+                    pb.up()
+            pb.fine()
 
         print("writing")
         pb = Progress(length=len(cohorts))
@@ -54,26 +57,29 @@ def process_path(path_in, path_out, force, tag_old, tag_new, cohort, categorical
             cohorts_id = list()
             cohorts_meta = dict()
             paths = list()
-            pb = Progress()
             writer = MultiFileWriter()
-            with gzip.open(path_in, "rt") as f_in:
-                for text, meta in iter_s(f_in, level=tag_old):
-                    cohort_id = "_".join([meta[c] for c in categorical])
-                    cohort_meta = {c: meta[c] for c in categorical}
-                    path = os.path.join(tmp_dir, cohort_id + ".vrt.gz")
-                    if cohort_id not in cohorts_meta.keys():
-                        cohorts_meta[cohort_id] = cohort_meta
-                        cohorts_meta[cohort_id]['id'] = cohort_id
-                        cohorts_id.append(cohort_id)
-                        paths.append(path)
-                    writer.write(path, dict2meta(meta, level=tag_new))
-                    writer.write(path, "\n".join(text) + "\n")
-                    writer.write(path, f"</{tag_new}>" + "\n")
-                    pb.up()
-            writer.close()
-            pb.fine()
 
-            print("collecting")
+            for path_in in paths_in:
+                print(path_in)
+                pb = Progress()
+                with gzip.open(path_in, "rt") as f_in:
+                    for text, meta in iter_s(f_in, level=tag_old):
+                        cohort_id = "_".join([meta[c] for c in categorical])
+                        cohort_meta = {c: meta[c] for c in categorical}
+                        path = os.path.join(tmp_dir, cohort_id + ".vrt.gz")
+                        if cohort_id not in cohorts_meta.keys():
+                            cohorts_meta[cohort_id] = cohort_meta
+                            cohorts_meta[cohort_id]['id'] = cohort_id
+                            cohorts_id.append(cohort_id)
+                            paths.append(path)
+                        writer.write(path, dict2meta(meta, level=tag_new))
+                        writer.write(path, "\n".join(text) + "\n")
+                        writer.write(path, f"</{tag_new}>" + "\n")
+                        pb.up()
+                writer.close()
+                pb.fine()
+
+            print("collecting and writing")
             pb = Progress(length=len(paths))
             with gzip.open(path_out, "wt") as f_out:
                 f_out.write("<corpus>\n")
@@ -89,11 +95,13 @@ def process_path(path_in, path_out, force, tag_old, tag_new, cohort, categorical
 def main(args):
     """"""
 
-    process_path(args.path_in,
-                 args.path_out,
-                 args.force,
-                 args.tag_old,
-                 args.tag_new,
-                 args.tag_cohort,
-                 args.categorical,
-                 args.memory)
+    paths_in = glob(args.glob_in)
+
+    process_paths(paths_in,
+                  args.path_out,
+                  args.force,
+                  args.tag_old,
+                  args.tag_new,
+                  args.tag_cohort,
+                  args.categorical,
+                  args.memory)
